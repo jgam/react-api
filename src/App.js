3742f8ac2baa9609619efc97002ddb99f6e44e53
jgam <jgam@alumni.nd.edu>
2020-04-09 14:30:11 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Router from 'Components/Router';
-import Header from "Components/Header";
 
 function App() {
   return (
     <div className="App">
       <>
-      <Header />
         <Router />
       </>
     </div>
