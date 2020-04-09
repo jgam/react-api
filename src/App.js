c2ca8b98a0853bffbaca6d58237a120f988ff08b
jgam <jgam@alumni.nd.edu>
2020-04-09 14:58:46 +0900

    header divided

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Router from 'Components/Router';
+import GlobalStyles from './Components/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
       <>
         <Router />
+        <GlobalStyles />
       </>
-    </div>
   );
 }
 
