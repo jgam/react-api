bb0fa2a26bccc0edcdd3126f41822e9e0cc5a833
jgam <jgam@alumni.nd.edu>
2020-04-10 22:56:01 +0900

    movie api done

--- README.md
+++ README.md
@@ -11,8 +11,10 @@ Learning React and ES6 by building a Movie Discovery App.
 
 ## API Verbs
 
-- [] Now Playing (Movie)
-- [] Upcoming (Movie)
-- [] Top Rated (TV, MOvie)
-- [] Popiular (TV, Movie)
-- [] Airing Today (TV)
+- [x] Now Playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV, MOvie)
+- [x] Popiular (TV, Movie)
+- [x] Airing Today (TV)
+- [x] TV show
+- [x] Movie Detail
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,33 +1,32 @@
-import React from "react";
+import React from 'react';
 //import styles from "./Header.module.css";
 
 import styled from 'styled-components';
-import {Link, withRouter} from 'react-router-dom';
-
+import { Link, withRouter } from 'react-router-dom';
 
 const Header = styled.header`
   color: white;
   position: fixed;
-  top:0;
+  top: 0;
   left: 0;
   width: 100%;
   height: 50px;
   display: flex;
   align-items: center;
-  background-color:rgba(20, 20, 20, 0.8);
+  background-color: rgba(20, 20, 20, 0.8);
 
   z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
-
 `;
 const List = styled.ul`
-  display:flex;
-`
+  display: flex;
+`;
 const Item = styled.li`
   width: 80px;
   height: 50px;
   text-align: center;
-  border-bottom: 5px solid ${props => props.current? "#3498db": "transparent"};
+  border-bottom: 5px solid
+    ${(props) => (props.current ? '#3498db' : 'transparent')};
 `;
 const SLink = styled(Link)`
   height: 50px;
@@ -36,17 +35,17 @@ const SLink = styled(Link)`
   justify-content: center;
 `;
 
-export default withRouter(({location: {pathname}}) =>(
+export default withRouter(({ location: { pathname } }) => (
   <Header>
     <List>
-      <Item current={pathname === "/"}>
-        <SLink to="/">Moviesss</SLink>
+      <Item current={pathname === '/'}>
+        <SLink to='/'>Movies</SLink>
       </Item>
-      <Item current={pathname ==="/tv"}>
-        <SLink to="/tv">TV</SLink>
+      <Item current={pathname === '/tv'}>
+        <SLink to='/tv'>TV</SLink>
       </Item>
-      <Item current={pathname ==="/search"}>
-        <SLink to="/search">Search</SLink>
+      <Item current={pathname === '/search'}>
+        <SLink to='/search'>Search</SLink>
       </Item>
     </List>
   </Header>
