2981139d089c23948786c8b9d6e5395a1e68ad76
jgam <jgam@alumni.nd.edu>
2020-04-11 22:14:28 +0900

    detail container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import SearchPresenter from './SearchPresenter';
+import { tvApi } from 'api';
+import { moviesApi } from '../../api';
 
 export default class extends React.Component {
   state = {
     movieResults: null,
     tvResults: null,
-    searchTerm: '',
+    searchTerm: 'code',
     loading: false,
     error: null,
   };
 
+  componentDidMount() {
+    console.log('component did mount');
+    this.handleSubmit();
+  }
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== '') {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      console.log(movieResults, tvResults);
+      this.setState({ movieResults, tvResults });
+    } catch {
+      this.setState({
+        error: "can't find results!!",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
   render() {
     const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+    console.log(this.state);
     return (
       <SearchPresenter
         movieResults={movieResults}
@@ -19,6 +56,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
