bf522e0bd6b09f2b4641dfa48d11436d39edf39e
jgam <jgam@alumni.nd.edu>
2020-04-11 21:34:54 +0900

    api in container

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
+  baseURL: 'https://api.themoviedb.org/3/',
   params: {
     api_key: 'b8ea99bee79793ee5cbdb7caf126e4ea',
     language: 'en-US',
