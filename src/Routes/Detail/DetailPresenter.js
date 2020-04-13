381610ca1eb7b99741b0b0ca9c6307b122739da4
jgam <jgam@alumni.nd.edu>
2020-04-13 10:46:13 +0900

    when you click movie it is poster component and will link to different id url which also calls detail component

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -2,7 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const DetailPresenter = ({ result, loading, error }) => null;
+import Loader from '../../Components/Loader';
+
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left:0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${props => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+`;
+
+const DetailPresenter = ({ result, loading, error }) => (
+  loading ? <Loader /> : 
+  <Container>
+    <Backdrop bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}/>
+  </Container>
+);
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
